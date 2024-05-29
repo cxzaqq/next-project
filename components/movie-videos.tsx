56c9dc39152b80c12fd7900dbf6f6a52ffddb72b
cxzaqq <roy1228@naver.com>
2024-05-29 21:58:34 +0900

    finish

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,13 +1,24 @@
+import styles from "../styles/movie-vidoes.module.css";
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
-  //   await new Promise((resolve) => setTimeout(resolve, 3000));
-  //   throw new Error("something broke..");
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allowFullScreen
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        />
+      ))}
+    </div>
+  );
 }
